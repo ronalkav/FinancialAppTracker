fd917b730fb788aaaaccd0b0dcdef2df80a5cc30
ronalkav <ronalkav@gmail.com>
2024-02-10 10:25:49 -0600

    Finance App Tracker Files
    Files

---
+++ myapp/src/components/Transaction.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Transaction({transaction, deleteTransaction}) {
+    let sign = transaction.amount >= 0 ? "+" : "-";
+    return (
+        <li className={transaction.amount >= 0 ? "plus": "minus"}>
+            {transaction.text}
+            <span>
+                <span style = {{marginRight: 10}}>{transaction.date}</span>
+                {sign}${Math.abs(transaction.amount)}
+
+            </span>
+            <button className= "delete-btn" onClick={() => deleteTransaction(transaction.id)}> X </button>
+            
+         </li>
+    )
+}
---
+++ myapp/src/redux/reducer.js
@@ -0,0 +1,24 @@
+import { ADD_TRANSACTION, DELETE_TRANSACTION } from "./actions"
+
+const initialState = {
+    transactions: []
+}
+
+const expenseTrackerReducer = (state = initialState, action) => {
+    switch(action.type){
+        case ADD_TRANSACTION:
+            return {
+                ...state,
+                transactions: [action.payload, ...state.transactions]
+            }
+        case DELETE_TRANSACTION:
+            return {
+                ...state,
+                transactions: state.transactions.filter((transactions) => transactions.id !== action.payload)
+            }
+        default: return state 
+
+    }
+}
+
+export default expenseTrackerReducer
